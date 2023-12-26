da01f91028c00b23a7421c7221a5bd4240cbf659
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2023-12-26 20:40:18 +0300

    added some fixes to algorithm

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
-const slides = document.querySelectorAll(".slide");
+function slidesPlugin(activeSlide = 0) {
+  activeSlide -= 1;
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
+  const slides = document.querySelectorAll(".slide");
+
+  if (activeSlide < 0 || activeSlide >= slides.length) {
+    activeSlide = 0;
+  }
+
+  slides[activeSlide].classList.add("active");
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
 }
 
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
-    slide.classList.add("active");
-  });
-}
+slidesPlugin(2);
